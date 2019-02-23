ea992793927b36be6159b22e6a9aa8b66f29c650
Byeonggon Lee <gonny952@gmail.com>
2019-02-23 19:27:23 +0900

    Modify lexer.cc lexer.hh

--- lexer.cc
+++ lexer.cc
@@ -1,14 +1,7 @@
+#include <iostream>
 #include "lexer.hh"
 MyLexer::MyLexer() {}
 MyLexer::~MyLexer() {}
-/*
-void MyLexer::yy_switch_to_buffer(yy_buffer_state* newBuffer) {}
-yy_buffer_state* MyLexer::yy_create_buffer(std::istream* s, int size) {}
-yy_buffer_state* MyLexer::yy_create_buffer(std::istream& s, int size) {}
-void MyLexer::yy_delete_buffer(yy_buffer_state*) {}
-void MyLexer::yyrestart(std::istream* s) {}
-void MyLexer::yyrestart(std::istream& s) {}
-int MyLexer::yylex() {}
-void MyLexer::switch_streams(std::istream*, std::ostream*) {}
-void MyLexer::switch_streams(std::istream&, std::ostream&) {}
-*/
+void MyLexer::hello() {
+    std::cout << "HELLO WORLD" << std::endl;
+}
--- lexer.hh
+++ lexer.hh
@@ -6,15 +6,5 @@ class MyLexer : public yyFlexLexer {
 public:
     MyLexer();
     virtual ~MyLexer();
-    /*
-    virtual void yy_switch_to_buffer(yy_buffer_state* newBuffer);
-    virtual yy_buffer_state* yy_create_buffer(std::istream* s, int size);
-    virtual yy_buffer_state* yy_create_buffer(std::istream& s, int size);
-    virtual void yy_delete_buffer(yy_buffer_state*);
-    virtual void yyrestart(std::istream* s);
-    virtual void yyrestart(std::istream& s);
-    virtual int yylex();
-    virtual void switch_streams(std::istream*, std::ostream*);
-    virtual void switch_streams(std::istream&, std::ostream&);
-    */
+    void hello();
 };
