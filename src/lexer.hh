5413b263fe61d5259f75d5107a9500f4970dbf7c
Byeonggon Lee <gonny952@gmail.com>
2019-11-05 18:13:24 +0900

    Refactor

--- src/lexer.hh
+++ src/lexer.hh
@@ -9,16 +9,18 @@
 
 using json = nlohmann::json;
 namespace Gmcr {
-    class Lexer : public yyFlexLexer {
-    private:
-        json m_args;
-        lua_State* m_luaState;
-        std::stack<std::ifstream*> m_fileStack;
-        void pushArgs(const json&);
-    public:
-        Lexer(std::string&& argsFilePath, std::istream* new_in = nullptr, std::ostream* new_out = nullptr);
-        virtual ~Lexer();
-        virtual int yylex();
-        bool eval(std::string&&);
-    };
+    class Lexer;
+};
+
+class Gmcr::Lexer : public yyFlexLexer {
+private:
+    json m_args;
+    lua_State* m_luaState;
+    std::stack<std::ifstream*> m_fileStack;
+    void pushArgs(const json&);
+public:
+    explicit Lexer(std::string&& argsFilePath, std::istream* new_in = nullptr, std::ostream* new_out = nullptr);
+    virtual ~Lexer();
+    virtual int yylex();
+    bool eval(std::string&&);
 };
